a0c55c243d8ac00f206dbfb3cc3091da4604cb60
Rajnandini Pawar <prajnandini81@gmail.com>
2025-10-10 15:46:29 +0000

    givve button to chnage theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
   title: 'AirView',
@@ -14,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -28,10 +29,17 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased">
-        <FirebaseClientProvider>
-          {children}
-          <Toaster />
-        </FirebaseClientProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <FirebaseClientProvider>
+            {children}
+            <Toaster />
+          </FirebaseClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -4,20 +4,26 @@ import UserProfile from '@/components/dashboard/user-profile';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { PanelLeft } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { ThemeToggleButton } from '../theme-toggle';
 
 export default function DashboardHeader() {
   const isMobile = useIsMobile();
   return (
-    <header className="sticky top-0 z-10 flex h-16 items-center justify-between gap-4 border-b bg-background/80 px-4 backdrop-blur-sm sm:px-6">
+    <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center justify-between gap-4 border-b bg-background/80 px-4 backdrop-blur-sm sm:px-6">
       <div className="flex items-center gap-2">
-        {isMobile && <SidebarTrigger>
-          <Button variant="ghost" size="icon">
-            <PanelLeft />
-          </Button>
-        </SidebarTrigger>}
+        {isMobile && (
+          <SidebarTrigger>
+            <Button variant="ghost" size="icon">
+              <PanelLeft />
+            </Button>
+          </SidebarTrigger>
+        )}
         <h1 className="text-xl font-semibold">Dashboard</h1>
       </div>
-      <UserProfile />
+      <div className="flex items-center gap-2">
+        <ThemeToggleButton />
+        <UserProfile />
+      </div>
     </header>
   );
 }
