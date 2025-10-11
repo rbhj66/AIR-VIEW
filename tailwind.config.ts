b7c3beecffd877b7a17c8768bb248a8d92078397
Rajnandini Pawar <prajnandini81@gmail.com>
2025-10-11 14:28:03 +0000

    d diagram and best

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -101,11 +101,20 @@ export default {
             transform: 'translateX(20px)',
           },
         },
+        'pulse-wave': {
+          '0%': {
+            transform: 'translateX(-50%)',
+          },
+          '100%': {
+            transform: 'translateX(0%)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'flow-x': 'flow-x 1.5s linear infinite',
+        'pulse-wave': 'pulse-wave 3s linear infinite',
       },
     },
   },
